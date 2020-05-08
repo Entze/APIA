7072fd87f811964e24b0bb0e5a6a31347e68d189
John Meyer <meyerjm@miamioh.edu>
2020-05-08 21:33:45 +0000

    Draft lint script

---
+++ lint-import.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+clear
+echo 'Next: Adding vertical space between sentences'
+read -p "Press [Enter] to view continue"
+codemod --extensions tex '\. +' '.\n'
+
+clear
+echo 'Next: Converting smart quotes'
+read -p "Press [Enter] to view continue"
+codemod --extensions tex '“' '``'
+codemod --extensions tex '”' "''"
+
+clear
+echo 'Next: Converting Word lists'
+read -p "Press [Enter] to view continue"
+codemod --extensions tex '•\t' '    \\item '
+codemod --extensions tex '\t'
+
+clear
+echo 'Next: Converting citations'
+read -p "Press [Enter] to view continue"
+codemod --extensions tex '\([A-Za-z,. ]+? \d+(;[A-Za-z,. ]+? \d+)*\)'
+
+clear
+echo 'Next: Converting $math$ to $ math $'
+read -p "Press [Enter] to view continue"
+codemod --extensions tex '\$([^\$ ])(.*?)\$' '$ \1\2$'
+codemod --extensions tex '\$(.*?)([^\$ ])\$' '$\1\2 $'
