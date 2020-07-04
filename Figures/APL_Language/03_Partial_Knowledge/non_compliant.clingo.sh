30d1003e1e1a80e53fbbb51fcfd34ea6e0acee6d
John Meyer <meyerjm@miamioh.edu>
2020-07-03 21:23:11 -0400

    Don't use 'clingo 0'

--- Figures/APL_Language/03_Partial_Knowledge/non_compliant.clingo.sh
+++ Figures/APL_Language/03_Partial_Knowledge/non_compliant.clingo.sh
@@ -1,1 +1,1 @@
-echo ':- -permitted(assume_command(c1, m1)).' | clingo 0 Pm.lp delta_V_lim.lp D.lp SL.lp -
+echo ':- -permitted(assume_command(c1, m1)).' | clingo Pm.lp delta_V_lim.lp D.lp SL.lp -
--- Figures/APL_Language/03_Partial_Knowledge/weakly_compliant.clingo.sh
+++ Figures/APL_Language/03_Partial_Knowledge/weakly_compliant.clingo.sh
@@ -1,1 +1,1 @@
-echo ':- not -permitted(assume_command(c1, m1)).' | clingo 0 Pm.lp delta_V_lim.lp D.lp SL.lp -
+echo ':- not -permitted(assume_command(c1, m1)).' | clingo Pm.lp delta_V_lim.lp D.lp SL.lp -
