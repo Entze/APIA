7375bee6c6bfa2ff70a065010cd23950a39a34ea
John Meyer <meyerjm@miamioh.edu>
2020-07-06 18:42:21 -0400

    Update lint script

--- lint-import.sh
+++ lint-import.sh
@@ -4,6 +4,9 @@ clear
 echo 'Next: Adding vertical space between sentences'
 read -p "Press [Enter] to view continue"
 codemod --extensions tex '\. +' '.\n'
+codemod --extensions tex '(i\.e\.)\n' '\1'
+codemod --extensions tex '(e\.g\.)\n' '\1'
+codemod --extensions tex '(etc\.)\n' '\1'
 
 clear
 echo 'Next: Converting smart quotes'
@@ -28,7 +31,7 @@ read -p "Press [Enter] to view continue"
 codemod --extensions tex '\([A-Za-z,. ]+? \d+(;[A-Za-z,. ]+? \d+)*\)'
 
 clear
-echo 'Next: Converting $math$ to $ math $'
-read -p "Press [Enter] to view continue"
-codemod --extensions tex '\$([^\$ ])(.*?)\$' '$ \1\2$'
-codemod --extensions tex '\$(.*?)([^\$ ])\$' '$\1\2 $'
+# echo 'Next: Converting $math$ to $ math $'
+# read -p "Press [Enter] to view continue"
+# codemod --extensions tex '\$([^\$ ])(.*?)\$' '$ \1\2$'
+# codemod --extensions tex '\$(.*?)([^\$ ])\$' '$\1\2 $'
