a218a09ccedba4731b58326715c8efa1eb31744f
John Meyer <meyerjm@miamioh.edu>
2020-07-16 17:40:52 -0400

    Apply edits

--- Figures/AOPL_Language/01_Full_Knowledge/Scripts/01/weakly_compliant.clingo.sh
+++ Figures/AOPL_Language/01_Full_Knowledge/Scripts/01/weakly_compliant.clingo.sh
@@ -1,1 +1,1 @@
-cd ../.. && clingo --quiet Po.lp s0.lp instantiate_variables.lp A.lp Compliance_Checks/weakly_compliant.lp
+cd ../.. && clingo --quiet P.lp s0.lp instantiate_variables.lp A.lp Compliance_Checks/weakly_compliant.lp
