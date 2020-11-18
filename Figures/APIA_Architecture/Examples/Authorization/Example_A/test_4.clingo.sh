f314974aed6a0cdd3993e77e251b806ec6a0a06e
John Meyer <meyerjm@miamioh.edu>
2020-11-18 14:57:46 -0500

    Add fourth test for Example_A

---
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/test_4.clingo.sh
@@ -0,0 +1,2 @@
+clingo --const max_timestep=4 --const test=4 --warn=no-atom-undefined ../generic_encoding.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 0 \
+    | grep 'Answer:' -A1 | sed -n '2p' | tr ' ' '\n' | sort
