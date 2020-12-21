09c4518c0ce45a914273e29d090591475a52c6db
John Meyer <meyerjm@miamioh.edu>
2020-12-21 18:03:56 -0500

    Reorganize files

--- Figures/APIA_Architecture/Examples/Authorization/Example_A/test_2.clingo.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/test_2.clingo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-clingo --const max_timestep=4 --const test=2 --warn=no-atom-undefined ../generic_encoding.lp ../../../authorization_compliance.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 0 \
+clingo --const max_timestep=4 --const test=2 --warn=no-atom-undefined ../generic_encoding.lp ../../../AOPL_Util/authorization_compliance.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 0 \
     | grep 'Answer:' -A1 | sed -n '2p' | tr ' ' '\n' | sort
