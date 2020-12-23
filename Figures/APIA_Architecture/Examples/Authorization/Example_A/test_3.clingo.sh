9fafddd06d60a879e6b0b0ec44ee90ea0d981f2d
John Meyer <meyerjm@miamioh.edu>
2020-12-23 18:45:51 -0500

    Remove '--warn=no-atom-undefined'

--- Figures/APIA_Architecture/Examples/Authorization/Example_A/test_3.clingo.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/test_3.clingo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-clingo --const max_timestep=4 --const test=3 --warn=no-atom-undefined ../generic_encoding.lp ../../../AOPL_Util/authorization_compliance.lp ../../../aia_theory_of_intentions.lp ../../../aia_reasoning_tasks.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 0 \
+clingo --const max_timestep=4 --const test=3 ../generic_encoding.lp ../../../AOPL_Util/authorization_compliance.lp ../../../aia_theory_of_intentions.lp ../../../aia_reasoning_tasks.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 0 \
     | grep 'Answer:' -A1 | sed -n '2p' | tr ' ' '\n' | sort
