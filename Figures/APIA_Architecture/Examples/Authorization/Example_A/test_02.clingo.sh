b5c76faf9ca5689e21c85e5fd2a6c2a7d9f0dfff
John Meyer <meyerjm@miamioh.edu>
2020-12-23 18:57:34 -0500

    Refactor tests

--- Figures/APIA_Architecture/Examples/Authorization/Example_A/test_02.clingo.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/test_02.clingo.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-clingo --const max_timestep=4 --const test=2 ../generic_encoding.lp ../../../AOPL_Util/authorization_compliance.lp ../../../aia_theory_of_intentions.lp ../../../aia_reasoning_tasks.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 0 \
-    | grep 'Answer:' -A1 | sed -n '2p' | tr ' ' '\n' | sort
+./test.sh 2
