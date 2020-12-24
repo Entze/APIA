c121b2b509abfce301a005e33fa8499f2a76070f
John Meyer <meyerjm@miamioh.edu>
2020-12-24 16:33:34 -0500

    Only produce one answer set

--- Figures/APIA_Architecture/Examples/Authorization/Example_B/test.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_B/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-clingo --opt-mode=optN --const max_timestep=4 --const test="$1" ../generic_encoding.lp ../../../AOPL_Util/authorization_compliance.lp ../../../aia_theory_of_intentions.lp ../../../aia_reasoning_tasks.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 0 \
+clingo --opt-mode=optN --const max_timestep=4 --const test="$1" ../generic_encoding.lp ../../../AOPL_Util/authorization_compliance.lp ../../../aia_theory_of_intentions.lp ../../../aia_reasoning_tasks.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 1 \
     | grep 'Answer:' -A1 | tail -n 2 | sed -n '2p' | tr ' ' '\n' | sort
