9adc07e1f2ba2e668bce21eff56a9e5be055c166
John Meyer <meyerjm@miamioh.edu>
2021-02-02 21:09:51 -0500

    Add diff script

---
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/diff_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if (( $# != 2 )); then
+    echo "Usage: $0 PREVIOUS_TEST_NUMBER NEW_TEST_NUMBER" >&2
+    exit 1
+fi
+PREVIOUS_TEST_NUMBER=$1
+NEW_TEST_NUMBER=$2
+
+PREVIOUS_TEST_FILE=$(mktemp)
+NEW_TEST_FILE=$(mktemp)
+
+./test.sh "${PREVIOUS_TEST_NUMBER}" > "${PREVIOUS_TEST_FILE}"
+./test.sh "${NEW_TEST_NUMBER}" > "${NEW_TEST_FILE}"
+git --no-pager diff --no-index "${PREVIOUS_TEST_FILE}" "${NEW_TEST_FILE}"
+
+rm "${PREVIOUS_TEST_FILE}" "${NEW_TEST_FILE}"
