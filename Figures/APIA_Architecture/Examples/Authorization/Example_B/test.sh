0a91ac18652e845f8a1860f67148501846226cd5
John Meyer <meyerjm@miamioh.edu>
2021-02-19 10:09:44 -0500

    Add all files in scripts

--- Figures/APIA_Architecture/Examples/Authorization/Example_B/test.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_B/test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-clingo --opt-mode=optN --const max_timestep=4 --const test="$1" ../../../misc_dynamic_domain.lp ../../../aopl_authorization_compliance.lp ../../../aia_theory_of_intentions.lp ../../../aia_reasoning_tasks.lp domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 1 \
+GLOBAL_FILES=(  aaa_axioms.lp  aia_reasoning_tasks.lp  aia_theory_of_intentions.lp  aopl_authorization_compliance.lp  aopl_obligation_compliance.lp  misc_dynamic_domain.lp  )
+clingo --opt-mode=optN --const max_timestep=4 --const test="$1" "${GLOBAL_FILES[@]}" domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 1 \
     | grep 'Answer:' -A1 | tail -n 2 | sed -n '2p' | tr ' ' '\n' | sort
