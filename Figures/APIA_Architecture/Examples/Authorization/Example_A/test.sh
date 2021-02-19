9e069781596f9704d72f9e26ccc0343164a6eb71
John Meyer <meyerjm@miamioh.edu>
2021-02-19 10:30:16 -0500

    Refactor test script

--- Figures/APIA_Architecture/Examples/Authorization/Example_A/test.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/test.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-GLOBAL_FILES=(  aaa_axioms.lp  aia_reasoning_tasks.lp  aia_theory_of_intentions.lp  aopl_authorization_compliance.lp  aopl_obligation_compliance.lp  misc_dynamic_domain.lp  )
-clingo --opt-mode=optN --const max_timestep=4 --const test="$1" "${GLOBAL_FILES[@]}" domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp 1 \
-    | grep 'Answer:' -A1 | tail -n 2 | sed -n '2p' | tr ' ' '\n' | sort
+../../../test.sh "$1" domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp
---
+++ Figures/APIA_Architecture/test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if (( $# < 2 )); then
+    echo "Usage: $0 FILE [...]" >&2
+    exit 1
+fi
+
+TEST_NUM=$1
+shift
+FILES=( "$@" )
+
+GLOBAL_FILES=(
+    aaa_axioms.lp
+    aia_reasoning_tasks.lp
+    aia_theory_of_intentions.lp
+    aopl_authorization_compliance.lp
+    aopl_obligation_compliance.lp
+    misc_dynamic_domain.lp
+)
+
+for GLOBAL_FILE in "${GLOBAL_FILES[@]}"; do
+    RELATIVE_PATH=$(realpath --relative-to . "${SCRIPT_DIR}/${GLOBAL_FILE}")
+    FILES+=( "${RELATIVE_PATH}" )
+done
+
+clingo --opt-mode=optN --const max_timestep=4 --const test="${TEST_NUM}" "${FILES[@]}" 1 \
+    | grep 'Answer:' -A1 | tail -n 2 | sed -n '2p' | tr ' ' '\n' | sort
