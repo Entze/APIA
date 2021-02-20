d77194fe2a90998fdf318ee7776c1d6be7886833
John Meyer <meyerjm@miamioh.edu>
2021-02-20 16:55:35 -0500

    Allow interactive input in scripts

--- Figures/APIA_Architecture/Examples/Authorization/Example_A/diff_test.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/diff_test.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-if (( $# != 2 )); then
-    echo "Usage: $0 PREVIOUS_TEST_NUMBER NEW_TEST_NUMBER" >&2
-    exit 1
+if (( $# == 2 )); then
+    PREVIOUS_TEST_NUMBER=$1
+    NEW_TEST_NUMBER=$2
+else
+    if tty -s; then
+        read -r -p 'Previous test number: ' PREVIOUS_TEST_NUMBER
+        read -r -p 'New test number: ' NEW_TEST_NUMBER
+    else
+        echo "Usage: $0 PREVIOUS_TEST_NUMBER NEW_TEST_NUMBER" >&2
+        exit 1
+    fi
 fi
-PREVIOUS_TEST_NUMBER=$1
-NEW_TEST_NUMBER=$2
 
 PREVIOUS_TEST_FILE=$(mktemp "test_${PREVIOUS_TEST_NUMBER}.XXXXXXXXXX")
 NEW_TEST_FILE=$(mktemp "test_${NEW_TEST_NUMBER}.XXXXXXXXXX")
--- Figures/APIA_Architecture/Examples/Authorization/Example_A/test.sh
+++ Figures/APIA_Architecture/Examples/Authorization/Example_A/test.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-../../../test.sh "$1" domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp
+
+if (( $# == 1 )); then
+    TEST_NUM=$1
+else
+    if tty -s; then
+        read -r -p 'Test number: ' TEST_NUM
+    else
+        echo "Usage: $0 TEST_NUM" >&2
+        exit 1
+    fi
+fi
+
+../../../test.sh "${TEST_NUM}" domain_encoding.lp instance.lp ../sanity_checks.lp tests.lp
