41f8ccbd5b13f986e782ab1e79dff45d7a64368c
John Meyer <meyerjm@miamioh.edu>
2021-03-03 12:44:59 -0500

    Lint script

--- lint-import.sh
+++ lint-import.sh
@@ -2,7 +2,7 @@
 
 clear
 echo 'Next: Adding vertical space between sentences'
-read -p "Press [Enter] to view continue"
+read -r -p "Press [Enter] to view continue"
 codemod --extensions tex '\. +' '.\n'
 codemod --extensions tex '(i\.e\.)\n' '\1'
 codemod --extensions tex '(e\.g\.)\n' '\1'
@@ -12,7 +12,7 @@ codemod --extensions tex '(e\.g\.) +' '\1~'
 
 clear
 echo 'Next: Converting smart quotes'
-read -p "Press [Enter] to view continue"
+read -r -p "Press [Enter] to view continue"
 codemod --extensions tex '“' '``'
 codemod --extensions tex '”' "''"
 codemod --extensions tex '‘' '`'
@@ -23,17 +23,17 @@ codemod --extensions tex '\\ldots' "\dots"
 
 clear
 echo 'Next: Converting Word lists'
-read -p "Press [Enter] to view continue"
+read -r -p "Press [Enter] to view continue"
 codemod --extensions tex '•\t' '    \\item '
 codemod --extensions tex '\t'
 
 clear
 echo 'Next: Converting citations'
-read -p "Press [Enter] to view continue"
+read -r -p "Press [Enter] to view continue"
 codemod --extensions tex '\([A-Za-z,. ]+? \d+(;[A-Za-z,. ]+? \d+)*\)'
 
 clear
 # echo 'Next: Converting $math$ to $ math $'
-# read -p "Press [Enter] to view continue"
+# read -r -p "Press [Enter] to view continue"
 # codemod --extensions tex '\$([^\$ ])(.*?)\$' '$ \1\2$'
 # codemod --extensions tex '\$(.*?)([^\$ ])\$' '$\1\2 $'
