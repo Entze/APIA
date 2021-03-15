902cfc9bdd75ef6b3a89b173413b1452d57b5455
John Meyer <meyerjm@miamioh.edu>
2021-03-15 12:55:35 -0400

    Move utility functions

--- Figures/APIA_Architecture/test.py
+++ Figures/APIA_Architecture/test.py
@@ -20,6 +20,17 @@ class AIALoopStep(IntEnum):
     OBSERVE_WORLD = 4
 
 
+def str_format(template_str: clingo.Symbol, *arguments: clingo.Symbol) -> str:
+    template_str = template_str.string
+    arguments = (symbol.string if symbol.type == clingo.SymbolType.String else symbol
+                 for symbol in arguments)
+    return template_str.format(*arguments)
+
+
+def function_signature(symbol: clingo.Symbol) -> str:
+    return f'{symbol.name}/{len(symbol.arguments)}'
+
+
 def generate_aia_subprograms_to_ground(current_timestep: int, max_timestep: int, step_number: AIALoopStep):
     # base
     yield ASPSubprogramInstantiation(name='base', arguments=())
