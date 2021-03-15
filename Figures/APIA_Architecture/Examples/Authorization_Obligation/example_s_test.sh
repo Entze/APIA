064e416c7358ae21f577c1ca52ad95f41a1642c3
John Meyer <meyerjm@miamioh.edu>
2021-03-14 21:20:19 -0400

    Add Example_S script

---
+++ Figures/APIA_Architecture/Examples/Authorization_Obligation/example_s_test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if (( $# >= 1 )); then
+    TEST_NUM=$1
+    MAX_TIMESTEP=$2
+else
+    if tty -s; then
+        read -r -p 'Test number: ' TEST_NUM
+    else
+        echo "Usage: $0 TEST_NUM MAX_TIMESTEP" >&2
+        exit 1
+    fi
+fi
+
+FILES=(
+    example_s.lp
+    Departments/domain_encoding.lp
+    Departments/example_s.lp
+    Departments/instance.lp
+    Meetings/domain_encoding.lp
+    Meetings/example_s.lp
+    Meetings/instance.lp
+    Movement/domain_encoding.lp
+    Movement/example_s.lp
+    Movement/instance.lp
+    Objects/domain_encoding.lp
+    Objects/example_s.lp
+    Objects/instance.lp
+)
+
+../../test.sh "${TEST_NUM}" "${MAX_TIMESTEP:-12}" "${FILES[@]}"
