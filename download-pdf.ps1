b7bc1f7892e3150b5c888db45888e5d1a7dd777a
John Meyer <meyerjm@miamioh.edu>
2021-03-15 16:47:40 -0400

    Make copy for Microsoft Word

--- download-pdf.ps1
+++ download-pdf.ps1
@@ -1,6 +1,7 @@
 while ($true) {
     try {
         Invoke-WebRequest http://localhost:8000/Thesis.pdf -O Thesis.pdf
+        Copy-Item Thesis.pdf Thesis_Word.pdf
         Invoke-Item .\Thesis.pdf
     } catch {
         Write-Host $_
