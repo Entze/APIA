5d27e8994d3a093ba906f4e461542278ef00ec9d
John Meyer <meyerjm@miamioh.edu>
2021-03-27 11:31:15 -0400

    Convert spaces to '_'

--- Figures/APIA_Architecture/test.py
+++ Figures/APIA_Architecture/test.py
@@ -79,7 +79,7 @@ def str_format(template_str: clingo.Symbol, *arguments: clingo.Symbol) -> str:
     template_str = template_str.string
     arguments = (symbol.string if symbol.type == clingo.SymbolType.String else symbol
                  for symbol in arguments)
-    return template_str.format(*arguments)
+    return template_str.format(*arguments).replace(' ', '_')
 
 
 def function_signature(symbol: clingo.Symbol) -> str:
