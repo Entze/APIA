5e6596fb27523178becb1ae5ee49015080107a08
John Meyer <meyerjm@miamioh.edu>
2021-04-08 17:35:07 -0400

    Add 'run.sh' files

---
+++ Figures/APIA_Architecture/Examples/Authorization_Obligation/Example_T/run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+MAX_TIMESTEP=12
+
+cd "${SCRIPT_DIR}" || exit 2
+
+FILES=(
+    ../example_t.lp
+
+    ../Departments/domain_encoding.lp
+    ../Departments/example_s.lp
+    ../Departments/instance.lp
+
+    ../Meetings/domain_encoding.lp
+    ../Meetings/example_s.lp
+    ../Meetings/instance.lp
+
+    ../Movement/domain_encoding.lp
+    ../Movement/example_s.lp
+    ../Movement/instance.lp
+
+    ../Objects/domain_encoding.lp
+    ../Objects/example_s.lp
+    ../Objects/instance.lp
+)
+
+../../../Implementation/apia_control_loop.py --max-timestep "${MAX_TIMESTEP}" "${FILES[@]}" "$@"
---
+++ Figures/APIA_Architecture/Examples/Obligation/Example_O/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+MAX_TIMESTEP=12
+
+cd "${SCRIPT_DIR}" || exit 2
+
+FILES=(
+    domain_encoding.lp
+    instance.lp
+    run_observations.lp
+)
+
+../../../Implementation/apia_control_loop.py --max-timestep "${MAX_TIMESTEP}" "${FILES[@]}" "$@"
